012b44230e40d2b01164f7ea279d442e55745b85
Stéphane Restani <stephane.restani@epfl.ch>
2015-09-11 09:40:25 +0200

    initial commit

---
+++ leaflet-legend.js
@@ -0,0 +1,28 @@
+
+L.Control.Legend = L.Control.extend({
+
+	initialize: function (layersSymbologies, options) {
+		L.setOptions(this, options);
+		
+		this.layersSymbologies = layersSymbologies;
+	},
+
+	onAdd: function (map) {
+	    var div = L.DomUtil.create('div', 'info legend')
+
+	    for(var s in this.layersSymbologies) {
+		    div.innerHTML += "<p>" + s + "</p>";
+
+		    var symbology = this.layersSymbologies[s];
+		    for (var symbol in symbology) {
+		        div.innerHTML +=
+		            '<i style="' + symbology[symbol] + '"></i> ' +
+		            symbol + "<br />";
+		    }
+		}
+	}
+});
+
+L.control.legend = function (layersSymbologies, options) {
+    return new L.Control.Legend(layersSymbologies, options);
+};
