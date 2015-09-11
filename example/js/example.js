b034e81848c4d25449b9189355f0bffcdfb850d6
Stéphane Restani <stephane.restani@epfl.ch>
2015-09-11 13:17:23 +0200

    updated example

--- example/js/example.js
+++ example/js/example.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([46.113327, 7.073485], 18);
+var map = L.map('map').setView([46.113327, 7.073485], 10);
 
 // Display a basemap
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
@@ -6,14 +6,14 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
 // Initialize and add the legend control
 var symbology = {
-    "<b>Heating system</b>": {
-        "Oil":"background:#E60000",
-        "Gas":"background:#FFAA00"
+    "<b>Layer 1</b>": {
+        "Symbol A":"background:#E60000",
+        "Symbol B":"background:#FFAA00"
     },
-    "<b>Recommended heating system</b>": {
-        "District heating network":"background:#8400A8;opacity:0.4", 
-        "Water heatpump":"background:#894444;opacity:0.4", 
-        "Ground heatpump":"background:#4CE600;opacity:0.4"
+    "<b>Layer 2</b>": {
+        "Semi transparent symbol A":"background:#8400A8;opacity:0.4", 
+        "Semi transparent symbol B":"background:#894444;opacity:0.4", 
+        "Semi transparent symbol C":"background:#4CE600;opacity:0.4"
     }
 };
 
